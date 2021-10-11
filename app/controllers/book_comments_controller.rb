9dc064a3bbbee759fed07b341eaff6ef46e48efd
syunki4224 <syunkikoike4224@gmail.com>
2021-10-11 12:29:42 +0000

    非同期通信の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,23 @@
 class BookCommentsController < ApplicationController
+
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    if comment.save
-       redirect_back(fallback_location: root_path)
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
     else
-       redirect_back(fallback_location: root_path)
-    end
   end
-  
+  end
+
   def destroy
-    BookComment.find_by(id: params[:id]).destroy
-    redirect_to request.referer
-  end 
-  
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy
+  end
+
   private
-  
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end 
-  
+   params.require(:book_comment).permit(:comment)
+  end
+
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,23 @@
 class FavoritesController < ApplicationController
+
+  before_action :book_params
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_back(fallback_location: root_path) 
+   book = Book.find(params[:book_id])
+   favorite = current_user.favorites.new(book_id: book.id)
+   favorite.save
   end
-  
+
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+   book = Book.find(params[:book_id])
+   favorite = current_user.favorites.find_by(book_id: book.id)
+   favorite.destroy
   end
+
+  private
+ #books/show画面で定義した＠bookを反映させるための記述
+  def book_params
+    @book = Book.find(params[:book_id])
+  end
+
 end
